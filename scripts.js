cf9cd29751159a33f774d36adec927b6e6ff5733
sachin-gautam <gautamsachin757@gmail.com>
2024-07-27 10:58:11 +0545

    added the css for the buttons

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ let a;
 let b;
 let o;
 
-const digits=[0,1,2,3,4,5,6,7,8,9];
 
 function add(a,b){
     return a+b;
@@ -42,17 +41,11 @@ function operate(a,b,o){
 }
 
 const output = document.querySelector("#result");
-output.textContent = 'Result';
+output.textContent = '0';
+
 const buttons = document.querySelectorAll("button");
 
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const input = button.textContent;
-        while(input!='')
-        output.textContent = input;
-    })
-});
 
 
 
